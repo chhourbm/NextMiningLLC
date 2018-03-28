44a5a7f1dfecaedf4d1f57232267144e1d4cf0e6
rmonh <36209453+rmonh@users.noreply.github.com>
2018-03-28 15:52:13 -0400

    Added placeholder for product details, updated team info

--- func.js
+++ func.js
@@ -58,3 +58,24 @@ frmvalidator.addValidation("comments","req","Please provide your email");
 frmvalidator.addValidation("email","email",
   "Please enter a valid email address");
 
+//Product detail tabs 
+function productDetail(evt, productDetailTabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(productDetailTabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
